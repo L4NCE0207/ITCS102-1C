d22827931a2d9bc98fdae4211e6f5550dc81b45d
L4NCE0207 <lancedanielfrancisco6@gmail.com>
2025-08-18 21:16:58 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,8 @@
+a = 5
+b = 5
+
+name1 = 'lance'
+name2 = 'francisco'
+
+print(a <= b)
+print(a > b)
---
+++ activity9.py
@@ -0,0 +1,14 @@
+a = 3
+b = 2
+
+c = 6
+d = 7
+
+username = 'lance'
+password = 'lance123'
+
+print("USER LOGIN ")
+print( (username == 'lance') and (password == 'lance123'))
+
+#print( (a > b) and (c < d) )
+#print( (a < b) or (c < d) )
