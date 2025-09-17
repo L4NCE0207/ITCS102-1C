e32fc20bb711605ce7c929c3b262eed5a506fd13
L4NCE0207 <lancedanielfrancisco6@gmail.com>
2025-09-17 23:19:59 +0800

    Add files via upload

---
+++ code_challenge9.py
@@ -0,0 +1,7 @@
+print("Hello, welcome to Countdown Timer Simulator")
+number = eval(input("Enter the starting number for countdown: "))
+
+for x in range(number, 0, -1):
+    print(x)
+
+print("Liftoff!")
